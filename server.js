9e678030c85de549ee3e6333e99532a933c1d349
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-10-27 00:41:04 +0530

    Add File Support
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- server.js
+++ server.js
@@ -3,18 +3,26 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const admin = require("firebase-admin");
-const multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
+const multer = require('multer')
+var storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now() + '.jpeg');
+  }
+});
+var upload = multer({storage: storage});
 
 const serviceAccount = require("./HealthHistory-3992571e7843.json");
 var gcs = require('@google-cloud/storage')({
-    projectId: 'healthhistory-459fe',
-    keyFilename: './HealthHistory-3992571e7843.json'
+  projectId: 'healthhistory-459fe',
+  keyFilename: './HealthHistory-3992571e7843.json'
 });
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://healthhistory-459fe.firebaseio.com/"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://healthhistory-459fe.firebaseio.com/"
 });
 
 
@@ -26,95 +34,94 @@ let database = admin.database().ref();
 
 
 function addFileToStorage(userId, file) {
-    let time = new Date().getTime();
-    var bucket = admin.storage().bucket("/" + userId + "/" + "images");
+  let time = new Date().getTime();
+  var bucket = admin.storage().bucket("/" + userId + "/" + "images");
 
-    newFileName = time ;
-    let fileUpload = bucket.file(newFileName);
+  newFileName = time;
+  let fileUpload = bucket.file(newFileName);
 
-    const blobStream = fileUpload.createWriteStream({
-        metadata: {
-            contentType: file.mimetype
-        }
-    });
+  const blobStream = fileUpload.createWriteStream({
+    metadata: {
+      contentType: file.mimetype
+    }
+  });
 
-    blobStream.on('error', (error) => {
-        reject('Something is wrong! Unable to upload at the moment.');
-    });
+  blobStream.on('error', (error) => {
+    reject('Something is wrong! Unable to upload at the moment.');
+  });
 
-    blobStream.on('finish', () => {
-        // The public URL can be used to directly access the file via HTTP.
-        const url = format(`https://storage.googleapis.com/${bucket.name}/${fileUpload.name}`);
-        resolve(url);
-    });
+  blobStream.on('finish', () => {
+    // The public URL can be used to directly access the file via HTTP.
+    const url = format(`https://storage.googleapis.com/${bucket.name}/${fileUpload.name}`);
+    resolve(url);
+  });
 
-    blobStream.end(file.buffer);
+  blobStream.end(file.buffer);
 
 
 }
 
 
+function getUserDetails(userId, cb) {
 
- function getUserDetails(userId, cb) {
+  let userRef = database.child("users").child(userId);
 
-    let userRef = database.child("users").child(userId);
+  userRef.on("value", function (snapshot) {
 
-    userRef.on("value", function (snapshot) {
+    userRef.off("value");
+    cb(snapshot.val());
 
-        userRef.off("value");
-        cb(snapshot.val());
-
-    }, function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
-        cb(false);
-    });
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+    cb(false);
+  });
 }
 
-function addFileLinkToUser(userId, title, time,fileLink) {
+function addFileLinkToUser(userId, title, time, fileLink) {
 
-    let userRef = database.child("users").child(userId);
-    let newImageRef = userRef.child("userImages").child(time);
-    newImageRef.set({
-        description: "",
-        imgType: "5",
-        timeStamp: time,
-        title: title,
-        url: fileLink
-    });
-    notifyUser(userRef);
+  let userRef = database.child("users").child(userId);
+  let newImageRef = userRef.child("userImages").child(time);
+  newImageRef.set({
+    description: "",
+    imgType: "5",
+    timeStamp: time,
+    title: title,
+    url: fileLink
+  });
+  notifyUser(userRef);
 }
 
 function notifyUser(userRef, title) {
-    userRef.child("firebaseInsstanceId").on("value", function (snapshot) {
-        console.log(snapshot.val());
-        let registrationToken = snapshot.val();
+  userRef.child("firebaseInsstanceId").on("value", function (snapshot) {
+    console.log(snapshot.val());
+    let registrationToken = snapshot.val();
 
-        // See the "Defining the message payload" section below for details
-        // on how to define a message payload.
-        var payload = {
-            "notification": {
-                "body": "Reports are ready",
-                "title": title,
-                "sound": "default"
-            }
-        };
+    // See the "Defining the message payload" section below for details
+    // on how to define a message payload.
+    var payload = {
+      "notification": {
+        "body": "Reports are ready",
+        "title": title,
+        "sound": "default"
+      }
+    };
 
-        // Send a message to the device corresponding to the provided
-        // registration token.
-        admin.messaging().sendToDevice(registrationToken, payload)
-            .then(function (response) {
-                // See the MessagingDevicesResponse reference documentation for
-                // the contents of response.
-                console.log("Successfully sent message:", response);
-            })
-            .catch(function (error) {
-                console.log("Error sending message:", error);
-            });
+    // Send a message to the device corresponding to the provided
+    // registration token.
+    admin.messaging().sendToDevice(registrationToken, payload)
+      .then(function (response) {
+        // See the MessagingDevicesResponse reference documentation for
+        // the contents of response.
+        console.log("Successfully sent message:", response);
+      })
+      .catch(function (error) {
+        console.log("Error sending message:", error);
+      });
 
-        userRef.child("firebaseInsstanceId").off("value");
-    }, function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
-    });
+    userRef.child("firebaseInsstanceId").off("value");
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
 }
 
 
@@ -126,20 +133,24 @@ app.use(cors());
 
 app.get('/user/:userid', function (req, res, next) {
   console.log(req.params.userid);
-  let user =  getUserDetails(req.params.userid, function (user) {
-    if(!user){
+  let user = getUserDetails(req.params.userid, function (user) {
+    if (!user) {
       return res.send("No User Found.")
-    };
-    console.log(user);
+    }
+    ;
+    // console.log(user);
     res.send(user);
   });
 });
 
-app.post('/user/:userid',  upload.single('avatar'),function (req, res, next) {
+app.post('/user/:userid', upload.single('avatar'), function (req, res, next) {
+  console.log('****************');
+  console.log(req.file);
+  res.send({success: true})
 
 });
 
 
 app.listen(5555, function () {
-    console.log("server start at port http://localhost:" + 5555)
+  console.log("server start at port http://localhost:" + 5555)
 });
