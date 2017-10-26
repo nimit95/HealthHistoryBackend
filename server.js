f32ef8c89deffb818f078f5e7182598a8cae02fd
Nimit Aggarwal <nimitagg95@gmail.com>
2017-10-26 12:31:01 +0530

    send notification

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express');
 
 const app = express();
@@ -21,6 +19,58 @@ admin.initializeApp(functions.config().firebase);
 // Get a reference to the database service
 let database = admin.database();
 
-app.listen(5555, function(){
-    console.log("server start at port http://localhost:"+5555)
+
+function addFileToStorage(userId, title) {
+
+}
+
+function addFileLinkToUser(userId, title, fileLink) {
+    let time = new Date().getTime();
+    let userRef = database.child("users").child(userId);
+    let newImageRef = userRef.child("userImages").child(time);
+    newImageRef.set({
+        description: "",
+        imgType: "5",
+        timeStamp: time,
+        title: title,
+        url: fileLink
+    });
+    notifyUser(userRef);
+}
+
+function notifyUser(userRef, title) {
+    userRef.child("firebaseInsstanceId").on("value", function (snapshot) {
+        console.log(snapshot.val());
+        let registrationToken = snapshot.val();
+
+        // See the "Defining the message payload" section below for details
+    // on how to define a message payload.
+        var payload = {
+            "notification" : {
+                "body" : "Reports are ready",
+                "title" : title
+            }
+        };
+
+        // Send a message to the device corresponding to the provided
+        // registration token.
+        admin.messaging().sendToDevice(registrationToken, payload)
+            .then(function (response) {
+                // See the MessagingDevicesResponse reference documentation for
+                // the contents of response.
+                console.log("Successfully sent message:", response);
+            })
+            .catch(function (error) {
+                console.log("Error sending message:", error);
+            });
+
+        ref.off("value");
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+}
+
+
+app.listen(5555, function () {
+    console.log("server start at port http://localhost:" + 5555)
 });
